f2aebf961b80f06a894b76a47ddc6877e1517f74
M Riezky Nurfajri <riezkynurfajri@gmail.com>
2022-07-02 11:37:23 +0700

    edited buttons for admin

--- admin/src/components/CompanyList.js
+++ admin/src/components/CompanyList.js
@@ -13,12 +13,12 @@ export default function CompanyList() {
 
   return (
     <div className="relative pt-12 mx-auto">
-      <table className="bg-transparent w-4/5 mx-auto">
+      <table className="bg-transparent w-auto mx-auto">
         <thead className="bg-white">
           <tr>
             <th className="px-4 w-24 py-4 text-xs text-zinc-700 rounded-tl-xl">Id</th>
             <th className="px-4 w-24 py-4 text-xs text-zinc-700 ">Name</th>
-            <th className="px-4 w-36 py-4 text-xs text-zinc-700">Logo</th>
+            <th className="px-12 w-auto py-4 text-xs text-zinc-700">Logo</th>
             <th className="px-4 w-56 py-4 text-xs text-zinc-700">Location</th>
             <th className="px-4 w-48 py-4 text-xs text-zinc-700">Email</th>
             <th className="px-4 w-96 py-4 text-xs text-zinc-700">Description</th>
@@ -31,22 +31,30 @@ export default function CompanyList() {
               <td className="px-4 py-4 text-xs text-zinc-700">{company.id}</td>
               <td className="px-4 py-4 text-xs text-zinc-700">{company.name}</td>
               <td className="px-4 py-4 text-xs text-zinc-700">
-                <img src={company.companyLogo} alt="logo" className="w-24 h-24" />
+                <img
+                  src={company.companyLogo}
+                  alt="logo"
+                  className="w-24 h-24 shadow-lg shadow-gray-700 rounded-full"
+                />
               </td>
               <td className="px-4 py-4 text-xs text-zinc-700">{company.location}</td>
               <td className="px-4 py-4 text-xs text-zinc-700">{company.email}</td>
-              <td className="px-4 py-4 text-xs text-zinc-700">{company.description}</td>
-              <td>
+              <td className="px-4 py-4 text-xs text-zinc-700">
+                {company.description.split(" ").slice(0, 12).join(" ")} . . .
+              </td>
+              <td className="flex px-4 py-12 justify-center">
                 <Link
                   to={`/companies/${company.id}`}
-                  className="text-zinc-200 mr-2 px-2 bg-emerald-600 rounded-xl"
+                  className="text-zinc-200 mr-2 px-2  bg-emerald-600 rounded-lg shadow-lg shadow-emerald-300 hover:bg-white hover:text-emerald-500 hover:shadow-emerald-300 duration-500"
                 >
                   See Details
                 </Link>
-                <button className="text-zinc-200 mr-2 px-2 bg-sky-600 rounded-xl">Edit</button>
+                <button className="text-zinc-200 mr-2 px-2 bg-sky-600 rounded-lg shadow-lg shadow-sky-300 bg-opacity-80 hover:text-sky-500 hover:bg-white hover:scale-110 duration-500">
+                  Edit
+                </button>
                 <button
                   onClick={() => removeCompany(company.id)}
-                  className="text-zinc-200 px-2 bg-rose-600 rounded-xl"
+                  className="text-zinc-200 px-2 bg-rose-600 rounded-lg bg-opacity-80 shadow-lg shadow-rose-300 hover:text-rose-500 hover:bg-white hover:scale-110 duration-500"
                 >
                   Delete
                 </button>
